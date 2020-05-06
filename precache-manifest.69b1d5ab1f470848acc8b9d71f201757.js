05b2a2ed5d890950a51b2f0be17d4dd7913f2f02
williamklingler <1klinglerwil@hdsb.ca>
2020-05-06 18:40:49 -0400

    initial commit

---
+++ precache-manifest.69b1d5ab1f470848acc8b9d71f201757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40bce79c011cd6fba499",
+    "url": "/static/js/main.40bce79c.chunk.js"
+  },
+  {
+    "revision": "3c39ba42ad25ca369680",
+    "url": "/static/js/1.3c39ba42.chunk.js"
+  },
+  {
+    "revision": "40bce79c011cd6fba499",
+    "url": "/static/css/main.21716c83.chunk.css"
+  },
+  {
+    "revision": "e7205dd8ff577175f6a0c419d6f154ae",
+    "url": "/index.html"
+  }
+];
